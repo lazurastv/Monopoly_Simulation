0781cab388b7921d29cfdb4b0eded970754db90b
lazurastv <dominikwawrzyniuk@gmail.com>
2021-04-15 22:43:54 +0200

    Houses, Mortgages

--- Player.py
+++ Player.py
@@ -3,17 +3,19 @@ class Player:
 
     def __init__(self, money, position):
         self.money = money
-        self.id = Player.id
         self.position = position
+        self.id = Player.id
         Player.id += 1
 
-    def __str__(self):
-        return "Player " + str(self.id) + ": $" + str(self.money)
-
     def pay(self, amount, player=None):
+        amount //= 1
         self.money -= amount
         if player:
             player.earn(amount)
 
     def earn(self, amount):
+        amount //= 1
         self.money += amount
+
+    def __str__(self):
+        return "Player " + str(self.id) + ": $" + str(self.money)
--- Property.py
+++ Property.py
@@ -7,6 +7,7 @@ class Property:
         self.house_price = house_price
         self.rent = rent
         self.mortgage = mortgage
+        self.mortgaged = False
         self.houses = 0
         self.owner = None
         self.id = Property.id
@@ -16,5 +17,22 @@ class Property:
         if self.owner:
             player.pay(self.owner)
 
-    def mortgage(self):
-        pass
+    def take_mortgage(self):
+        if self.owner and not self.mortgaged:
+            self.owner.earn(self.mortgage)
+            self.mortgaged = True
+
+    def pay_mortgage(self):
+        if self.owner and self.mortgaged:
+            self.owner.pay(self.mortgage * 1.1)
+            self.mortgaged = False
+
+    def buy_house(self):
+        if self.owner and self.owner.can_pay(self.house_price) and self.houses < 5:
+            self.owner.pay(self.house_price)
+            self.houses += 1
+
+    def sell_house(self):
+        if self.owner and self.houses > 0:
+            self.owner.earn(self.house_price / 2)
+            self.houses -= 1
