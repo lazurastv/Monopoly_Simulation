dedb7d886d53251f7ae7589714c019de01609406
lazurastv <dominikwawrzyniuk@gmail.com>
2021-06-05 01:30:19 +0200

    Trading via need Graph traversal, early consider_position method

--- ai/trading/sequence_alg/trader.py
+++ ai/trading/sequence_alg/trader.py
@@ -1,13 +1,17 @@
+from copy import deepcopy
+
+
 class Trader:
     def __init__(self, groups, player_count):
         self.groups = groups
         self.player_count = player_count
-        self.bars = []
+        self.nodes = []
+        self.positions = []
 
     def solve_for(self, player):
-        self.complete_group([player], self.groups, {})
+        self.get_valid_positions([player], self.groups, {})
 
-    def complete_group(self, players, groups, bars, index=0):
+    def get_valid_positions(self, players, groups, nodes, index=0):
         if len(players) == self.player_count:
             return
         target = players[index]
@@ -15,53 +19,90 @@ class Trader:
             groups_copy = groups.copy()
             groups_copy.remove(group)
             players_copy = players.copy()
-            bars_copy = bars.copy()
+            nodes_copy = nodes.copy()
             for tile, player in group:
                 if player != target:
-                    if target not in bars_copy:
-                        bars_copy[target] = {player: [tile]}
-                    elif player not in bars_copy[target]:
-                        bars_copy[target][player] = [tile]
+                    if target not in nodes_copy:
+                        nodes_copy[target] = {player: [tile]}
+                    elif player not in nodes_copy[target]:
+                        nodes_copy[target][player] = [tile]
                     else:
-                        bars_copy[target][player].append(tile)
+                        nodes_copy[target][player].append(tile)
                     if player not in players_copy:
                         players_copy.append(player)
             if index + 1 == len(players_copy):
-                self.bars.append(bars_copy)
+                self.nodes.append(nodes_copy)
             else:
-                self.complete_group(players_copy, groups_copy, bars_copy, index + 1)
+                self.get_valid_positions(players_copy, groups_copy, nodes_copy, index + 1)
 
-    def construct_solution(self, players, bars):
-        for player in players:
-            exclusions = []
-            failed = False
-            sol = None
-            for problem in bars[player]:
-                sol = self.move(player, problem, bars, [], exclusions)
-                if sol is None:
-                    failed = True
-                    break
-                exclusions.append(problem)
-            if not failed:
-                return sol
-
-    def move(self, start, end, bars, sequence, exclude=None):
-        bars_copy = bars.copy()
-        start_problems = bars_copy.pop(start)
-        for node in bars_copy:
-            if start in bars_copy[node] and node not in start_problems:
-                sequence_copy = sequence.copy()
-                sequence_copy.append(bars_copy[node][start])
-                if node == end and (exclude is None or node not in exclude):
-                    # sequence_copy.append()
-                    return sequence_copy
+    def walk(self, source, dest, nodes, next_graph):
+        nodes_copy = deepcopy(nodes)
+        current_node = nodes_copy.pop(source)
+        for node in current_node:
+            if node in nodes_copy:
+                next_graph_copy = deepcopy(next_graph)
+                if len(next_graph_copy[source]) > 1:
+                    next_graph_copy[source].pop(node)
                 else:
-                    return self.move(node, end, bars_copy, sequence_copy, exclude)
+                    next_graph_copy.pop(source)
+                if node == dest:
+                    self.positions.append([source, current_node[node], next_graph_copy])
+                else:
+                    self.walk(node, dest, nodes_copy, next_graph_copy)
+
+    def consider_position(self, position, player):
+        bars = []
+        for bar in position[player]:
+            bars.append(bar)
+        positions = [0, 0, position]
+        while len(bars) > 0:
+            bar = bars.pop()
+            for _, _, pos in positions:
+                self.walk(bar, player, pos, pos)
+                for posit in self.positions:
+                    last_node = posit[0]
+                    tiles = position[last_node][player]
+                    try:
+                        posit[2][bar][last_node].append(*tiles)
+                    except KeyError:
+                        posit[2][bar][last_node] = tiles
 
 
 example = [[(0, 0), (1, 0), (2, 2)], [(3, 1), (4, 1), (5, 2)], [(6, 0), (7, 1), (8, 2)], [(9, 0), (10, 1), (11, 0)]]
 solver = Trader(example, 4)
 solver.solve_for(0)
-solution = solver.bars[0]
+solution = solver.nodes[0]
 print(solution)
-print(solver.construct_solution([0, 1, 2], solution))
+solver.walk(0, 1, solution, solution.copy())
+print(solver.positions)
+
+"""
+
+    def find_trade(self, source, visited, groups, trades):
+        if len(visited) == self.player_count:
+            return
+        for group in groups:
+            groups_copy = groups.copy()
+            groups_copy.remove(group)
+            trades_copy = trades.copy()
+            for tile, player in group:
+                if source == self.start and player != self.start:
+                    try:
+                        trades_copy[player].insert(tile, 0)
+                    except KeyError:
+                        trades_copy[player] = [tile, "<->"]
+                elif source != self.start and player == self.start:
+                    trades_copy.append(tile)
+                elif player != source:
+                    visited_copy = visited.copy()
+                    visited_copy.append(player)
+                    self.find_trade(player, visited_copy, groups_copy, trades_copy[player])
+            if isinstance(trades_copy, dict):
+                for player in trades_copy:
+                    visited_copy = visited.copy()
+                    visited_copy.append(player)
+                    self.find_trade(player, visited_copy, groups_copy, trades_copy[player])
+                if trades_copy[source][-1] != "<->":
+                    self.trades.append(trades_copy)
+
+"""
